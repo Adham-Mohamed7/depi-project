e19ef416e5e5423ba1438d1e5636eee5a3ac3488
Adham-Mohamed7 <professionalg93@gmail.com>
2025-04-20 03:37:47 +0200

    fix error2

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/home" element={<Home />} />
+      <Route path="/HomePage" element={<HomePage />} />
     </Routes>
     </div>
     
