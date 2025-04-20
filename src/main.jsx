c8210ef5efb8c09222b93ff81b548cbecdef4c00
Seif <saifmohamed2004144@gamil.com>
2025-04-20 17:24:06 +0200

    seif

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ContactUs from './src/contact.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <ContactUs />
   </StrictMode>,
 )
