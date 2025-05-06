9a8ae3fb7dc15c797f7c1c5dd6a81e2cad64ca83
saramahmoud25 <saramahmou12@gmail.com>
2025-05-06 12:47:57 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import SignUp from "./form/SignUp";
 import Login from "./form/Login";
-import HomePage from "./HomePage/HomePage"
+import HomePage from "./HomePage/HomePage";
 import DashBoard from './DashboardDesignAndView/DashBoard';
 import Shop from './Shopping/Shop'; 
 import Product from './Shopping/Product';
@@ -14,8 +14,8 @@ const App = () => {
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/login" element={<Login />} />
       <Route path="/HomePage" element={<HomePage />} />
-      <Route path="/shop" element={<Shopping />} />
-      <Route path="/Product" element={<Product/>}/>
+      <Route path="/shop" element={<Shop />} />
+      <Route path="/Product" element={<Product />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/dashboard" element={<DashBoard />} /> 
     </Routes>
