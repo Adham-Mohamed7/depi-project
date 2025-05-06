d44613885ea3967d8dfa7aebb23cba9779a1ed92
saramahmoud25 <saramahmou12@gmail.com>
2025-05-06 12:30:27 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
-import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import SignUp from "./form/SignUp";
 import Login from "./form/Login";
 import Home from "./Home";
-
-//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Product from './Shopping/Product';
-import Cart from './Shopping/Cart';
-
-
-
+import Shopping from './Shopping/Shop'; 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/Home" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/shop" element={<Shopping />} /> 
     </Routes>
   );
 };
