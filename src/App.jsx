bd74137bcb75c111ea0168d7f9e578b0a452f41e
Adham-Mohamed7 <150583508+Adham-Mohamed7@users.noreply.github.com>
2025-05-15 17:18:03 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { Routes, Route} from 'react-router-dom';
 import SignUp from './form/SignUp';
 import Login from './form/Login';
